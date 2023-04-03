c68d34ccb1ec15cc5f697ad0be85c8f0935f3e14
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-04-03 03:21:43 -0700

    add a fourth route : Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Prints the message 'Hello HBNB!' when '/' is called """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints the message 'HBNB' when '/hbnb' is called """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Prints the message 'C' when '/c/<text>' is called """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """ Prints the message 'Python' when '/python/<text>' is called """
+    return "Python " + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
